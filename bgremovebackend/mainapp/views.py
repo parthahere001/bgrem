0acf74cf9d7863cfbed6919126b366950508383a
parthahere001 <parthahere001.com>
2023-05-22 13:40:47 +0530

    [ADD] Image Handling

--- bgremovebackend/mainapp/views.py
+++ bgremovebackend/mainapp/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def homeView(request):
-    return render(request,'home.html')
+    return render(request,'stopkiddingaround.html')
+def uploadImageView(request):
+    if request.method == "POST":
+        dodo = 1;
+    else:
+        return render(request, "stopkiddingaround.html")
