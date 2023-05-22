9e7feb1b8adf1b3004c4efe6e622d1593c511071
parthahere001 <parthahere001.com>
2023-05-22 17:14:56 +0530

    [FIX] Image saving to DB

--- bgremovebackend/mainapp/views.py
+++ bgremovebackend/mainapp/views.py
@@ -1,9 +1,11 @@
+from io import BytesIO
 from django.shortcuts import render, redirect
 from rembg import remove
 from PIL import Image
 from django.http import HttpResponse
 from django.utils.datastructures import MultiValueDictKeyError
-
+from .models import imageStorageModel
+from django.core.files.base import ContentFile
 
 def homeView(request):
     return render(request,'home.html')
@@ -14,7 +16,14 @@ def uploadImageView(request):
             response = HttpResponse(content_type="image/png")
             input = Image.open(pic)
             output = remove(input)
-            output.save(response,'png')
+            fordb = output
+            img_io = BytesIO()
+            fordb.save(img_io, format='png', quality=100)
+            img_content = ContentFile(img_io.getvalue(), 'img.png')
+          
+            sampl = imageStorageModel(uploaded_image = pic, downloaded_image = img_content)
+            sampl.save()
+            output.save(response, 'png')
             return response
         except MultiValueDictKeyError:
             return render(request, "stopkiddingaround.html")
